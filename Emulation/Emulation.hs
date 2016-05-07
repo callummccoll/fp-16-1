da5a56243381611e7e067ff0bd18d54629392b40
andrewparoz <andyparoz@gmail.com>
2016-05-08 00:07:15 +1000

    More parts of the Emulation engine completed.

--- Emulation/Emulation.hs
+++ Emulation/Emulation.hs
@@ -2,104 +2,202 @@
 
 {-# LANGUAGE ScopedTypeVariables #-}
 
-module Main (main) where
+--module Main (main) where
 
+import Data.Array
+import Data.Array.IO
 import Environment
 
-	
+data Symbol = Symbol {
+	label :: String,
+	addr :: Int
+}
+	deriving Show
+
+symTable :: [Symbol]
+symTable = [(Symbol "X" 12), (Symbol "Main" 3)]
+
+
 main :: IO ()
 main = do
 	env <- loadExmapleEnv
-	env' <- freezeEnv env
 	
-	putStr (show env')
+	env' <- getExeStep env 15
+	
+	env'' <- freezeEnv env'
+	putStr (show env'')
 	return ()
 	
-{-
+getExeStep :: Environment -> Int -> IO Environment
+getExeStep env steps = case steps of
+	0 -> return env
+	_ -> do 
+		env' <- doExecutionStep env
+		getExeStep env' (steps-1)				
+
+doExecutionStep :: Environment -> IO Environment
+doExecutionStep env = do 
+	let Left ram = (eRAM env)
+	cell <- readArray ram (ePC env)
+	let inst = words (getCellInst cell)
+	putStr (show inst ++ "\n")
+	readInstruction inst env
+		
+readInstruction :: [String] -> Environment -> IO Environment
+readInstruction inst env = case inst of 
+	[] -> return env ----this means the step is greater than the program allows.
+	(x:xs) -> case x of
+		"HALT" -> return (actionHalt env)
+		"CALL" -> (actionCall (head xs) env)
+		"RETURN" -> actionReturn env
+		"MOVE" -> actionMove (head xs) (last xs) env
+		"ADD" -> actionAdd (head xs) (last xs) env
+		"SUB" -> actionSub (head xs) (last xs) env
+		"MULT" -> actionMult (head xs) (last xs) env
+		"DIV" -> return env
+		"MOD" -> return env
+		"JUMP" -> return env
+		"BEQ" -> return env
+		"BNE" -> return env
+		"BLT" -> return env
+		"BGT" -> return env
+		"BLE" -> return env
+		"BGE" -> return env
+		_ -> return env --invalid instruction?
 	
-getExeStep :: Machine -> Int -> IO Machine
-getExeStep machine steps = case steps of
-	0 -> return machine
-	_ -> getExeStep (doExecutionStep machine) (steps-1)				
-
-
-	
-doExecutionStep :: Machine -> Machine
-doExecutionStep machine = let
-		inst = getCurInstruction (stack machine) (regPC machine)
-	in case inst of 
-		[] -> machine ----this means the step is greater than the program allows.
-		(x:xs) -> case x of
-			"HALT" -> actionHalt machine
-			"CALL" -> actionCall (head xs) machine
-			"RETURN" -> actionReturn machine
-			"MOVE" -> actionMove (head xs) (last xs) machine
-			"ADD" -> machine
-			"SUB" -> machine
-			"MULT" -> machine
-			"DIV" -> machine
-			"MOD" -> machine
-			"JUMP" -> machine
-			"BEQ" -> machine
-			"BNE" -> machine
-			"BLT" -> machine
-			"BGT" -> machine
-			"BLE" -> machine
-			"BGE" -> machine
-			_ -> machine --invalid instruction?
-
---- Halting simply returns the same machine. No action is taken.
-actionHalt :: Machine -> Machine
+-- HALT: Halting simply returns the same machine. No action is taken.
+actionHalt :: Environment -> Environment
 actionHalt machine = machine
 
---set PC to addr given, then place return address into SP pointer, reduce SP.
-actionCall :: String -> Machine -> Machine
-actionCall addr machine = setDestValue "PC" (getSourceValue addr machine) $ setDestValue "SP" ((regSP machine)-1) $ setDestValue "(SP)" (regPC machine) machine
+-- CALL: set PC to addr given, then place return address into SP pointer, reduce SP.
+actionCall :: String -> Environment -> IO Environment
+actionCall lbl env = case lbl of
+	"print" -> incrementPC env
+	"read" -> incrementPC env
+	_ -> do
+		env' <- addToStack (ePC env) env
+		let addr' = (addr (getSymFromLabel lbl symTable))
+		setDestValue "PC" addr' env'
 
---take the addr from where SP is pointing and put that into the PC
-actionReturn :: Machine -> Machine
-actionReturn machine = setDestValue "SP" ((regSP machine)+1) $ setDestValue "PC" (regSP machine) machine
+-- RETURN: take the addr from where SP is pointing and put that into the PC
+actionReturn :: Environment -> IO Environment
+actionReturn env = do
+	v <- (getSourceValue "(SP)" env)
+	env' <- setDestValue "PC" v env
+	env'' <- setDestValue "SP" ((eSP env')+1) env'
+	env''' <- incrementPC env''
+	return env'''
 
---Moving a value from a source to a destination. 
-actionMove :: String -> String -> Machine -> Machine
-actionMove source dest machine = setDestValue "PC" ((regPC machine)+1) $ setDestValue dest (getSourceValue source machine) machine
+-- MOVE: Moving a value from src to dest. 
+actionMove :: String -> String -> Environment -> IO Environment
+actionMove src dest env = do
+	v <- getSourceValue src env
+	env' <- setDestValue dest v env
+	incrementPC env'
 
+-- Add: Adds dest to src and stores in dest
+actionAdd :: String -> String -> Environment -> IO Environment
+actionAdd src dest env = do
+		v1 <- getSourceValue src env
+		v2 <- getSourceValue dest env
+		env' <- setDestValue dest (v2 + v1) env
+		incrementPC env'
+	
+-- SUB: Subtracts dest from src and stores in dest
+actionSub :: String -> String -> Environment -> IO Environment
+actionSub src dest env = do
+		v1 <- getSourceValue src env
+		v2 <- getSourceValue dest env
+		env' <- setDestValue dest (v2 - v1) env
+		incrementPC env'
+		
+-- MULT: Multiplies dest with src and stores in dest
+actionMult :: String -> String -> Environment -> IO Environment
+actionMult src dest env = do
+		v1 <- getSourceValue src env
+		v2 <- getSourceValue dest env
+		env' <- setDestValue dest (v2 * v1) env
+		incrementPC env'
+		
 -----------------------------------------------------------
-getCurInstruction :: [MemBlock] -> Int -> Instruction
-getCurInstruction stack pc = case stack of
-	[] -> []
-	(x:xs) -> case pc of
-		0 -> words (value x)
-		_ -> getCurInstruction xs (pc-1)
-	
-setDestValue :: String -> Int -> Machine -> Machine
-setDestValue dest srcValue machine = case dest of
-	('(':xs) -> case xs of
-		"PC)" -> machine-- ?
-		"SP)" -> machine {stack = setIDestValue (regSP machine) srcValue (stack machine)}
-		"A)"  -> machine-- ?
-	"PC" -> machine {regPC = srcValue}
-	"SP" -> machine {regSP = srcValue}
-	"A" -> machine {regA = srcValue}
-	_ -> machine
+setDestValue :: String -> Int -> Environment -> IO Environment
+setDestValue dest srcValue env = case dest of
+	('#':xs) -> setIDestValue (convertStringToInt xs 0) srcValue env
+	"(PC)" -> setIDestValue (ePC env) srcValue env -- ?
+	"(SP)" -> setIDestValue (eSP env) srcValue env
+	"(A)"  -> setIDestValue (getIntFromCell(eA env)) srcValue env -- ?
+	"PC" -> return env {ePC = srcValue}
+	"SP" -> return env {eSP = srcValue}
+	"A" -> return env {eA = (getCellFromInt srcValue)}
+	xs -> case xs of		--at this point we have a nnn indreicton or symbol
+		('(':'P':'C':')':xs') -> setIDestValue ((ePC env)+(convertStringToInt xs' 0)) srcValue env
+		('(':'S':'P':')':xs') -> setIDestValue ((eSP env)+(convertStringToInt xs' 0)) srcValue env
+		('(':'A':')':xs') -> setIDestValue ((getIntFromCell(eA env))+(convertStringToInt xs' 0)) srcValue env
+		xs' -> setIDestValue (addr(getSymFromLabel xs' symTable)) srcValue env
 	--Need to add indrection calls -(...) +(...) (...)nnn etc
 	
-setIDestValue :: Int -> Int -> [MemBlock] -> [MemBlock]
-setIDestValue addr value stack = case stack of
-	[] -> []
-	(x:xs) -> case addr of
-		0 -> [MemBlock "" (show value)] ++ xs
-		_ -> [x] ++ setIDestValue (addr-1) value xs
+setIDestValue :: Int -> Int -> Environment -> IO Environment
+setIDestValue addr value env = let
+		Left ram = (eRAM env)
+	in do
+		--putStr ("=== " ++ (show addr) ++ " " ++ (show value) ++ "\n")
+		writeArray ram addr (getCellFromInt value)
+		return env
 	
-getSourceValue :: String -> Machine -> Int
-getSourceValue src machine = case src of
-	('#':xs) -> convertStringToInt xs 0
-	"PC" -> (regPC machine)
-	"SP" -> (regSP machine)
-	"A" -> (regA machine)
-	_ -> 0 -- Review this?
-	--Need to add indrection calls -(...) +(...) (...)nnn etc
+getSourceValue :: String -> Environment -> IO Int
+getSourceValue src env = case src of
+	('#':xs) -> return (convertStringToInt xs 0)
+	"(PC)" -> getIDestValue (ePC env) env
+	"(SP)" -> getIDestValue (eSP env) env
+	"(A)"  -> getIDestValue (getIntFromCell (eA env)) env
+	"PC" -> return (ePC env)
+	"SP" -> return (eSP env)
+	"A" -> return (getIntFromCell (eA env))
+	xs -> case xs of		--at this point we have a nnn indreicton or symbol
+		('(':'P':'C':')':xs') -> getIDestValue ((ePC env)+(convertStringToInt xs' 0)) env
+		('(':'S':'P':')':xs') -> getIDestValue ((eSP env)+(convertStringToInt xs' 0)) env
+		('(':'A':')':xs') -> getIDestValue ((getIntFromCell (eA env))+(convertStringToInt xs' 0)) env
+		xs' -> getIDestValue (addr(getSymFromLabel xs' symTable)) env
+	--Need to add indrection calls -(...) +(...) (...)nnn etc	
+	
+getIDestValue :: Int -> Environment -> IO Int
+getIDestValue addr env = let
+		Left ram = (eRAM env)
+	in do
+		--putStr ("=== " ++ (show addr) ++ "\n")
+		cell <- (readArray ram addr)
+		return (getIntFromCell cell)
+		
+getSymFromLabel :: String -> [Symbol] -> Symbol
+getSymFromLabel lbl syms = case syms of
+	(x:xs) -> if lbl == (label x)
+		then x
+		else getSymFromLabel lbl xs
+	
+-- A convenience function that puts an Int onto the stack and decrements the SP
+addToStack :: Int -> Environment -> IO Environment
+addToStack x env = do
+	env' <- setDestValue "SP" ((eSP env)-1) env
+	setDestValue "(SP)" x env'
+	
+-- A convenience function that increments the PC.
+incrementPC :: Environment -> IO Environment
+incrementPC env = setDestValue "PC" ((ePC env)+1) env
 
+	
+convertCharToInt :: Char -> Int
+convertCharToInt x = case x of
+		'0' -> 0
+		'1' -> 1
+		'2' -> 2
+		'3' -> 3
+		'4' -> 4
+		'5' -> 5
+		'6' -> 6
+		'7' -> 7
+		'8' -> 8
+		'9' -> 9
+	
 convertStringToInt :: String -> Int -> Int
 convertStringToInt xs num = case xs of
 	"" -> num
@@ -114,8 +212,7 @@ convertStringToInt xs num = case xs of
 		'7' -> convertStringToInt xs ((num * 10)+7)
 		'8' -> convertStringToInt xs ((num * 10)+8)
 		'9' -> convertStringToInt xs ((num * 10)+9)
-		
--}
+		_ -> convertStringToInt xs num
 
 
 		
--- Emulation/Environment.hs
+++ Emulation/Environment.hs
@@ -1,19 +1,30 @@
 {-# LANGUAGE ScopedTypeVariables #-}
 
-module Environment (Cell, Environment, initEnvF, thawEnv, freezeEnv, loadExmapleEnv) where
+module Environment (Cell, getCellInst, getIntFromCell, getCellFromInt, Environment, eA, eSP, ePC, eRAM, eStaticSize, eStdIn, eStdOut, initEnvF, thawEnv, freezeEnv, loadExmapleEnv) where
 
 import Data.Array
 import Data.Array.IO
 
 -- the number of cells, indexed from 0,
 memSize :: Int
-memSize = 10
+memSize = 18
+
+type Instruction = (String, Int, Int)
 
 data Cell = 
 	  Undefined
 	| Int Int
 	| Instruction String Int Int
 	deriving Show
+	
+getCellInst :: Cell -> String
+getCellInst (Instruction a b c) = a
+
+getIntFromCell :: Cell -> Int
+getIntFromCell (Int a) = a
+
+getCellFromInt :: Int -> Cell
+getCellFromInt x = Int x
 	
 data Environment = Environment {
 		eA :: Cell,
@@ -63,7 +74,25 @@ freezeEnv e = do
 
 loadExmapleEnv :: IO Environment
 loadExmapleEnv = do
-	ram :: IOArray Int Cell <- newArray (0,12) Undefined
+	ram :: IOArray Int Cell <- newArray (0, memSize) Undefined
+	writeArray ram 0 (Instruction "MOVE #1 X"		0 0)
+	writeArray ram 1 (Instruction "CALL Main" 		1 0)
+	writeArray ram 2 (Instruction "HALT" 			2 3)
+	writeArray ram 3 (Instruction "SUB #2 SP" 		3 3)
+	writeArray ram 4 (Instruction "MOVE #4 (SP)" 	4 5)
+	writeArray ram 5 (Instruction "MOVE (SP) A" 	5 5)
+	writeArray ram 6 (Instruction "ADD X A"		6 5)
+	writeArray ram 7 (Instruction "MOVE A (SP)1"	7 6)
+	writeArray ram 8 (Instruction "MOVE (SP)1 A"	8 6)
+	writeArray ram 9 (Instruction "CALL print" 		9 7)
+	writeArray ram 10 (Instruction "ADD #2 SP" 		10 7)
+	writeArray ram 11 (Instruction "RETURN" 		11 7)
+	
+	let e = initEnvF {eSP = memSize+1, eRAM = Left ram}
+	return e
+	
+	
+	{-
 	writeArray ram 0 (Instruction "MOV #1 X" 	0 0)
 	writeArray ram 1 (Instruction "CALL main" 	1 0)
 	writeArray ram 2 (Instruction "HALT" 		2 3)
@@ -77,9 +106,7 @@ loadExmapleEnv = do
 	writeArray ram 10 (Instruction "ADD #1 SP" 	10 7)
 	writeArray ram 11 (Instruction "ADD #2 SP" 	11 7)
 	writeArray ram 12 (Instruction "RETURN" 	12 7)
-	let e = initEnvF {eRAM = Left ram}
-	return e
-	
+	-}
 	
 	
 
