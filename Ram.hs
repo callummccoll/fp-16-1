14b0aa190996f933bb1b9f4af35be6e0a3cc3b96
Callum McColl <callum.mccoll1@gmail.com>
2016-06-15 15:25:36 +1000

    Colour PC and SP

--- Ram.hs
+++ Ram.hs
@@ -9,6 +9,10 @@ import Data.Array
 import Data.Array.MArray
 import Data.IORef
 
+type RowData = (Int, Maybe String, String)
+type Row = (Int, Frame, Frame, Frame)
+type RowFactory = RowData -> IO Row
+
 createRamAndRegisters:: Environment -> IO Frame
 createRamAndRegisters env = do
     case (eRAM env) of
@@ -16,40 +20,59 @@ createRamAndRegisters env = do
         Right ram -> getRamFromArray env ram
 
 getRamFromArray :: Environment -> (Array Int Cell) -> IO Frame
-getRamFromArray env ram = (extractRegisters env) >>= (createRam (extractCell <$> ram))
+getRamFromArray env ram = do
+    registers <- extractRegisters env
+    factory <- return (createRowWithColouredRegister
+            (ePC env, Color 30000 30000 30000)
+            (eSP env, Color 0 0 0)
+        )
+    createRam (extractRowData (elems ram) 0) factory registers
 
 extractRegisters :: Environment -> IO [(String, String)]
 extractRegisters env = do
     return [("A", showCVal (eA env)), ("SP", show (eSP env)), ("PC", show (ePC env)) ]
 
-extractCell :: Cell -> (Maybe String, String)
-extractCell c = case (cLabel c, cVal c) of
-    (l, Int c')  -> (Just l, (show c'))
-    (l, Inst c') -> (Just l, (showInstruction c'))
-    _            -> (Nothing, "")
+extractRowData :: [Cell] -> Int -> [RowData]
+extractRowData cs pos = case cs of
+    [] -> []
+    c : cs' -> case (cLabel c, cVal c) of
+        (l, Int c')  -> (pos, Just l, (show c')) : extractRowData cs' (pos + 1)
+        (l, Inst c') -> (pos, Just l, (showInstruction c')) : extractRowData cs' (pos + 1)
+        _            -> (pos, Nothing, "") : extractRowData cs' (pos + 1)
 
-createRam :: Array Int (Maybe String, String) -> [(String, String)] -> IO Frame
-createRam cs registers = do
+createRowWithColouredRegister :: (Int, Color) -> (Int, Color) -> RowData -> IO Row
+createRowWithColouredRegister (pc, pcColor) (sp, spColor) (row, label, content)
+    | row == pc = createRow (row, label, content) pcColor
+    | row == sp = createRow (row, label, content) spColor
+    | otherwise = createRow (row, label, content) (Color 65535 65535 65535)
+
+createRam :: [RowData] -> RowFactory -> [(String, String)] -> IO Frame
+createRam rs rowFactory registers = do
     hbox  <- hBoxNew False 10
-    (createRamTable cs) >>|> hbox <<|<< (createRegisters registers)
+    putStrLn "createRam"
+    (createRamTable rs rowFactory) >>|> hbox <<|<< (createRegisters registers)
     hbox >|>> (pad (5,5,5,5)) >>|>> (createFrame "Ram and Registers")
 
-createRamTable :: Array Int (Maybe String, String) -> IO Table
-createRamTable cs = do
-    table <- (tableNew (length cs) 3 False)
-    attachCellsToTable table (createRow <$> cs) 0
+createRamTable :: [RowData] -> RowFactory -> IO Table
+createRamTable rs rowFactory = do
+    putStrLn "createRamTable"
+    table <- (tableNew (length rs) 3 False)
+    rows <- return (rowFactory <$> rs)
+    putStrLn (show (length rows))
+    attachCellsToTable table rows
+    putStrLn "attached"
+    return table
 
-
-attachCellsToTable :: Table -> Array Int (IO (Frame, Frame)) -> Int -> IO Table
-attachCellsToTable table cells row
-    | row >= (length cells) = return table
-    | otherwise             = do
-        (label, content) <- cells ! row
-        rowCell <- createRowCell row
+attachCellsToTable :: Table -> [IO Row] -> IO ()
+attachCellsToTable table rs = case rs of
+    []      -> return ()
+    r : rs' -> do
+        putStrLn "attach"
+        (row, rowCell, labelFrame, contentFrame) <- r
         tableAttach table rowCell 0 1 row (row + 1) [Fill] [Fill] 0 0
-        tableAttach table label 1 2 row (row + 1) [Fill] [Fill] 0 0
-        tableAttach table content 2 3 row (row + 1) [Fill] [Fill] 0 0
-        attachCellsToTable table cells (row + 1)
+        tableAttach table labelFrame 1 2 row (row + 1) [Fill] [Fill] 0 0
+        tableAttach table contentFrame 2 3 row (row + 1) [Fill] [Fill] 0 0
+        attachCellsToTable table rs'
 
 createRowCell :: Int -> IO Frame
 createRowCell row = do
@@ -58,12 +81,13 @@ createRowCell row = do
         >>|>> (padWithAlignment (0, 0, 2, 2) (0.5, 0, 1, 1))
         >>|> frame
 
-createRow :: (Maybe String, String) -> IO (Frame, Frame)
-createRow (label, content) = do
+createRow :: RowData -> Color -> IO Row
+createRow (row, label, content) color = do
+    rowCell <- createRowCell row
     labelFrame <- frameNew
     widgetSetSizeRequest labelFrame 50 (-1)
     eventBox <- eventBoxNew
-    widgetModifyBg eventBox StateNormal (Color 65535 65535 65535)
+    widgetModifyBg eventBox StateNormal color
     widgetSetSizeRequest eventBox 80 (-1)
     label <- (labelNew (label))
         >>|>> (padWithAlignment (5, 5, 5, 5) (1, 0, 1, 1))
@@ -72,7 +96,7 @@ createRow (label, content) = do
         >>|>> (pad (5, 5, 5, 5))
         >>|> eventBox
         >>|>> (frameNew)
-    return (label, content)
+    return (row, rowCell, label, content)
 
 createRegisters :: [(String, String)] -> IO VBox
 createRegisters registers = do
