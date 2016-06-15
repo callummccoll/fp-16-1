72e9ff7be6326b5da8b9548146fca5515fa4b70a
Callum McColl <callum.mccoll1@gmail.com>
2016-06-15 15:34:36 +1000

    Changed colours of the cells for pc and sp and removed prints

--- Ram.hs
+++ Ram.hs
@@ -24,8 +24,8 @@ getRamFromArray env running ram = do
     registers <- extractRegisters env
     factory <- case running of
         True -> return (createRowWithColouredRegister
-                (ePC env, Color 30000 30000 30000)
-                (eSP env, Color 0 0 0)
+                (ePC env, Color 65535 64250 55769)
+                (eSP env, Color 55769 65535 64250)
             )
         False -> return (createRowWithColouredRegister
                 (ePC env, Color 65535 65535 65535)
@@ -54,25 +54,20 @@ createRowWithColouredRegister (pc, pcColor) (sp, spColor) (row, label, content)
 createRam :: [RowData] -> RowFactory -> [(String, String)] -> IO Frame
 createRam rs rowFactory registers = do
     hbox  <- hBoxNew False 10
-    putStrLn "createRam"
     (createRamTable rs rowFactory) >>|> hbox <<|<< (createRegisters registers)
     hbox >|>> (pad (5,5,5,5)) >>|>> (createFrame "Ram and Registers")
 
 createRamTable :: [RowData] -> RowFactory -> IO Table
 createRamTable rs rowFactory = do
-    putStrLn "createRamTable"
     table <- (tableNew (length rs) 3 False)
     rows <- return (rowFactory <$> rs)
-    putStrLn (show (length rows))
     attachCellsToTable table rows
-    putStrLn "attached"
     return table
 
 attachCellsToTable :: Table -> [IO Row] -> IO ()
 attachCellsToTable table rs = case rs of
     []      -> return ()
     r : rs' -> do
-        putStrLn "attach"
         (row, rowCell, labelFrame, contentFrame) <- r
         tableAttach table rowCell 0 1 row (row + 1) [Fill] [Fill] 0 0
         tableAttach table labelFrame 1 2 row (row + 1) [Fill] [Fill] 0 0
